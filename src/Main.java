ccd406019a79b5b67189d1f60c9f839b931581d4
phucgari <phucgari@gmail.com>
2023-01-13 16:13:27 +0700

    done

--- src/Main.java
+++ src/Main.java
@@ -16,25 +16,44 @@ public class Main {
         books[7]=new FictionBook("ground",100,"cousin","yeah3");
         books[8]=new FictionBook();
         books[9]=new FictionBook();
+
+        System.out.println("Java book number "+countProgrammingBookJava(books));
+        System.out.println("vien tuong book number "+countFictionVientuong1(books));
+        System.out.println("Fiction lowprice number "+countFictionLowPrice(books);
+        System.out.println("total price 10 books "+totalPrice(books));
+    }
+    public static double totalPrice(Book[] books){
         double totalPrice=0;
-        int countProgrammingBookJava=0;
-        int countFictionVientuong1=0;
-        int countFictionLowPrice=0;
         for (Book book:books) {
-            if(book instanceof ProgrammingBook&&((ProgrammingBook)book).getFramework()=="Java"){
+            totalPrice+=book.getPrice();
+        }
+        return totalPrice;
+    }
+    public static int countProgrammingBookJava(Book[] books){
+        int countProgrammingBookJava=0;
+        for (Book book:books) {
+            if (book instanceof ProgrammingBook && ((ProgrammingBook) book).getFramework() == "Java") {
                 countProgrammingBookJava++;
             }
+        }
+        return countProgrammingBookJava;
+    }
+    public static int countFictionVientuong1(Book[] books){
+        int countFictionVientuong1=0;
+        for (Book book:books) {
             if(book instanceof FictionBook&&((FictionBook)book).getCategory()=="Viễn tưởng 1"){
                 countFictionVientuong1++;
             }
+        }
+        return countFictionVientuong1;
+    }
+    public static int countFictionLowPrice(Book[] books){
+        int countFictionLowPrice=0;
+        for (Book book:books) {
             if(book instanceof FictionBook&&book.getPrice()<100){
                 countFictionLowPrice++;
             }
-            totalPrice+=book.getPrice();
         }
-        System.out.println("Java book number "+countProgrammingBookJava);
-        System.out.println("vien tuong book number "+countFictionVientuong1);
-        System.out.println("Fiction lowprice number "+countFictionLowPrice);
-        System.out.println("total price 10 books "+totalPrice);
+        return countFictionLowPrice;
     }
 }
